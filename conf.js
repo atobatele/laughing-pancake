82e77790dd1ce102324e0aea7b3b140603dafa5e
Kayode Atobatele <atobatele@yahoo.com>
2020-04-07 02:32:40 -0400

    added jasmine and jasmine-reporter

---
+++ conf.js
@@ -0,0 +1,26 @@
+exports.config = {
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  capabilities: {
+    browserName: 'chrome',
+  },
+  specs: ['specs/*_spec.js'],
+  framework: 'jasmine',
+  onPrepare: function () {
+    var jasmineReporters = require('jasmine-reporters');
+    var junitReporter = new jasmineReporters.JUnitXmlReporter({
+
+        // setup the output path for the junit reports
+      savePath: 'output/',
+
+        // conslidate all true:
+        //   output/junitresults.xml
+        //
+        // conslidate all set to false:
+        //   output/junitresults-example1.xml
+        //   output/junitresults-example2.xml
+      consolidateAll: false,
+
+    });
+    jasmine.getEnv().addReporter(junitReporter);
+  },
+};
